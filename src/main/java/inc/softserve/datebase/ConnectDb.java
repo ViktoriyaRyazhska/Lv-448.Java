0910c25747739985ee63eb7993d56a06e16d56ad
user <w.turko3000@gmail.com>
2019-10-11 15:17:05 +0300

    Add connect to database.

---
+++ src/main/java/inc/softserve/datebase/ConnectDb.java
@@ -0,0 +1,28 @@
+package inc.softserve.datebase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDb {
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static final String CONNECT_URL = "jdbc:mysql://localhost:3306/" +
+            "travel_agency?" +
+            "useUnicode=true&" +
+            "useJDBCCompliantTimezoneShift=true&" +
+            "useLegacyDatetimeCode=false&" +
+            "serverTimezone=UTC";
+
+    public static Connection connectBase()  {
+        try{
+            Connection conn = DriverManager.getConnection(CONNECT_URL, USER, PASSWORD);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return conn;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
