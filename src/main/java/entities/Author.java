8880734b51e0541a7f9c3128dc761de218122988
Max Hrynkiv <maxym.grynkiv@gmail.com>
2019-10-13 15:50:14 +0300

    Added author class

---
+++ src/main/java/entities/Author.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.util.Objects;
+
+public class Author {
+
+    private long id;
+    private String authorFirstName;
+    private String authorLastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id &&
+                Objects.equals(authorFirstName, author.authorFirstName) &&
+                Objects.equals(authorLastName, author.authorLastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authorFirstName, authorLastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", authorFirstName='" + authorFirstName + '\'' +
+                ", authorLastName='" + authorLastName + '\'' +
+                '}';
+    }
+}
