d1ea0687656405e51b423fee1c45e0a868a572e9
Vitaliy_Dzen <vitaliy.dzen.303@gmail.com>
2019-10-15 17:28:25 +0300

    Update AudienceService -> add findByName(). Update AudienceServiceImpl -> add method findByName(), verification methods save, deleteById, update

--- src/main/java/academy/softserve/museum/dao/AudienceDao.java
+++ src/main/java/academy/softserve/museum/dao/AudienceDao.java
@@ -1,7 +1,10 @@
 package academy.softserve.museum.dao;
 
 import academy.softserve.museum.entities.Audience;
+import java.util.Optional;
 
 public interface AudienceDao extends Crud<Audience> {
+
+    Optional<Audience> findByName(String name);
 
 }
