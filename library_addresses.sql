8db600284b7a0d52e8f06fa5d6799e539adce734
Bogdan-Kuzenko <52287274+Bogdan-Kuzenko@users.noreply.github.com>
2019-10-17 11:59:59 +0300

    Delete library_addresses.sql

--- library_addresses.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO library.addresses (id, city, street, building_number, apartment, id_user) VALUES (1, 'Lviv', 'Horodocka', 1, 1, 1);
-INSERT INTO library.addresses (id, city, street, building_number, apartment, id_user) VALUES (2, 'Lviv', 'Franka', 1, 1, 2);
-INSERT INTO library.addresses (id, city, street, building_number, apartment, id_user) VALUES (3, 'Lviv', 'Bandery', 1, 1, 3);
-INSERT INTO library.addresses (id, city, street, building_number, apartment, id_user) VALUES (4, 'Lviv', 'Kulchycka', 1, 1, 4);
