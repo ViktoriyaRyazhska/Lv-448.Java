637dc84b2615c054ac086d2654784a156ba41db9
nomadness <vitalii.skolozdra@gmail.com>
2019-10-20 13:36:58 +0300

    Add util class for JSON serialization

--- src/main/java/academy/softserve/museum/util/Serializer.java
+++ src/main/java/academy/softserve/museum/util/Serializer.java
@@ -1,4 +1,19 @@
 package academy.softserve.museum.util;
 
-public class Serializer {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Serializer  {
+
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Method converts given object to JSON string
+     *
+     * @param obj object to serialize
+     * @return serialized JSON string
+     */
+    public static String toJsonString(Object obj) throws JsonProcessingException {
+        return mapper.writeValueAsString(obj);
+    }
 }
