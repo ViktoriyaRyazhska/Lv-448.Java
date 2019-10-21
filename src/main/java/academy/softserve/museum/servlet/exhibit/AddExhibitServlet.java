0d717a465bd88f6e902c111ed4316503234defa2
Vitaliy_Dzen <vitaliy.dzen.303@gmail.com>
2019-10-21 15:16:17 +0300

    Add opportunity create new exhibits. In servlet layer

--- src/main/java/academy/softserve/museum/servlet/exhibit/AddExhibitServlet.java
+++ src/main/java/academy/softserve/museum/servlet/exhibit/AddExhibitServlet.java
@@ -1,20 +1,18 @@
 package academy.softserve.museum.servlet.exhibit;
 
+import academy.softserve.museum.entities.Audience;
+import academy.softserve.museum.entities.Exhibit;
 import academy.softserve.museum.entities.ExhibitType;
 import academy.softserve.museum.services.AudienceService;
 import academy.softserve.museum.services.ExhibitService;
 import academy.softserve.museum.services.impl.AudienceServiceImpl;
 import academy.softserve.museum.services.impl.ExhibitServiceImpl;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @WebServlet("/exhibits/add-exhibit")
 public class AddExhibitServlet extends HttpServlet {
@@ -36,10 +34,24 @@ public class AddExhibitServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        String type = req.getParameter("type");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("name");
         String material = req.getParameter("material");
         String technic = req.getParameter("technic");
-        int audience = Integer.parseInt(req.getParameter("audience"));
+        ExhibitType exhibitType = ExhibitType.valueOf(req.getParameter("type"));
+
+        Audience audience = new Audience();
+        audience.setId(Long.parseLong(req.getParameter("audience")));
+        Exhibit exhibit = new Exhibit(exhibitType, material, technic, name);
+
+        try {
+            exhibitService.save(exhibit);
+            long id = exhibitService.findByName(exhibit.getName()).get().getId();
+            exhibit.setId(id);
+            exhibitService.updateExhibitAudience(exhibit, audience);
+            resp.sendRedirect(req.getContextPath() + "/exhibits");
+        } catch (RuntimeException e) {
+            resp.sendRedirect(req.getContextPath() + "/exhibits");
+        }
     }
 }
