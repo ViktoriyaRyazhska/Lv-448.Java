d4a43d4a18c420dc39e28b8502eb48383791056d
nomadness <vitalii.skolozdra@gmail.com>
2019-10-21 18:02:43 +0300

    Fix ui bugs

---
+++ src/main/java/academy/softserve/museum/servlet/IndexServlet.java
@@ -0,0 +1,16 @@
+package academy.softserve.museum.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = {"", "/museum"})
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/index.jsp").include(req, resp);
+    }
+}
