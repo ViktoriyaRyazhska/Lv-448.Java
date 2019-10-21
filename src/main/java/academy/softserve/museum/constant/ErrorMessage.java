fd8ae252a740173a262a686e020b308c9c571c86
Vitaliy_Dzen <vitaliy.dzen.303@gmail.com>
2019-10-21 18:27:47 +0300

    Create delete, save, update exceptions. Create class with error message constants. It will be updated

---
+++ src/main/java/academy/softserve/museum/constant/ErrorMessage.java
@@ -0,0 +1,15 @@
+package academy.softserve.museum.constant;
+
+public class ErrorMessage {
+    public static final String AUDIENCE_NOT_SAVED = "Audience not saved. Object with such name already exists. Id: ";
+    public static final String AUDIENCE_NOT_DELETED = "Audience not deleted. Object with such id does not exist. Id: ";
+    public static final String AUDIENCE_NOT_UPDATED = "Audience not updated. Object with such name does not exist. Id: ";
+
+    public static final String EXHIBIT_AUTHOR_NOT_SAVED = "Not saved. Objects Author and Exhibit with such id's already exists";
+    public static final String EXHIBIT_AUTHOR_NOT_DELETED = "Not deleted. Objects Author and Exhibit with such id's does not exist";
+
+    public static final String AUTHOR_NOT_SAVED = "Author not saved. Object with such first name and last name already exists. Id: ";
+    public static final String AUTHOR_NOT_DELETED = "Author not deleted. Object with such id does not exist. Id: ";
+    public static final String AUTHOR_NOT_UPDATED = "Author not updated. Object with such first name and last name does not exist. Id: ";
+
+}
