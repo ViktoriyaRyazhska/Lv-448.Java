d3948c56281397be905cdd5d7e8263ff13f949b3
Oleh Kopylchak <oleg12311999@gmail.com>
2019-10-23 00:21:58 +0300

    Added JavaDocs for statistic classes. Default constructors of statistic classes now initialize maps that that classes use

--- src/main/java/academy/softserve/museum/entities/statistic/EmployeeStatistic.java
+++ src/main/java/academy/softserve/museum/entities/statistic/EmployeeStatistic.java
@@ -6,17 +6,41 @@ import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class that contains Employee statistic
+ */
 public class EmployeeStatistic {
+
+    /**
+     * Map, that contains Employee as key and
+     * his / her work time as a value
+     */
     private Map<Employee, Integer> workTimeMap;
+    /**
+     * Map, that contains Employee as key and
+     * his / her excursion count as a value
+     */
     private Map<Employee, Integer> excursionCount;
+    /**
+     * statistic start date and time
+     */
     private Date dateStart;
+    /**
+     * statistic end date and time
+     */
     private Date dateEnd;
 
+    /**
+     * Constructor, that initialize maps
+     */
     public EmployeeStatistic() {
         workTimeMap = new HashMap<>();
         excursionCount = new HashMap<>();
     }
 
+    /**
+     * Constructor, that accepts all parameters
+     */
     public EmployeeStatistic(Map<Employee, Integer> workTimeMap, Map<Employee, Integer> excursionCount, Date dateStart, Date dateEnd) {
         this.workTimeMap = workTimeMap;
         this.excursionCount = excursionCount;
@@ -24,34 +48,58 @@ public class EmployeeStatistic {
         this.dateEnd = dateEnd;
     }
 
+    /**
+     * @return work time map
+     */
     public Map<Employee, Integer> getWorkTimeMap() {
         return workTimeMap;
     }
 
+    /**
+     * @return statistic start date and time
+     */
     public Date getDateStart() {
         return dateStart;
     }
 
+    /**
+     * @param dateStart statistic start date and time
+     */
     public void setDateStart(Date dateStart) {
         this.dateStart = dateStart;
     }
 
+    /**
+     * @return statistic end date and time
+     */
     public Date getDateEnd() {
         return dateEnd;
     }
 
+    /**
+     * @param dateEnd statistic end date and time
+     */
     public void setDateEnd(Date dateEnd) {
         this.dateEnd = dateEnd;
     }
 
+    /**
+     * @return excursion count map
+     */
     public Map<Employee, Integer> getExcursionCount() {
         return excursionCount;
     }
 
+    /**
+     * @param excursionCount excursion count map
+     */
     public void setExcursionCount(Map<Employee, Integer> excursionCount) {
         this.excursionCount = excursionCount;
     }
 
+    /**
+     * @param workTimeMap work time map
+     */
     public void setWorkTimeMap(Map<Employee, Integer> workTimeMap) {
         this.workTimeMap = workTimeMap;
     }
--- src/main/java/academy/softserve/museum/entities/statistic/ExhibitStatistic.java
+++ src/main/java/academy/softserve/museum/entities/statistic/ExhibitStatistic.java
@@ -1,31 +1,61 @@
 package academy.softserve.museum.entities.statistic;
 
+import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class, that contains Exhibit statistic
+ */
 public class ExhibitStatistic {
+    /**
+     * Map, that contains statistic by exhibit materials
+     */
     private Map<String, Integer> materialStatistic;
+    /**
+     * Map, that contains statistic by exhibit techniques
+     */
     private Map<String, Integer> techniqueStatistic;
 
+    /**
+     * Constructor, that accepts all parameters
+     */
     public ExhibitStatistic(Map<String, Integer> materialStatistic, Map<String, Integer> techniqueStatistic) {
         this.materialStatistic = materialStatistic;
         this.techniqueStatistic = techniqueStatistic;
     }
 
+    /**
+     * Constructor, that initialize maps
+     */
     public ExhibitStatistic() {
+        materialStatistic = new HashMap<>();
+        techniqueStatistic = new HashMap<>();
     }
 
+    /**
+     * @return material statistic map
+     */
     public Map<String, Integer> getMaterialStatistic() {
         return materialStatistic;
     }
 
+    /**
+     * @param materialStatistic material statistic map
+     */
     public void setMaterialStatistic(Map<String, Integer> materialStatistic) {
         this.materialStatistic = materialStatistic;
     }
 
+    /**
+     * @return technique statistic map
+     */
     public Map<String, Integer> getTechniqueStatistic() {
         return techniqueStatistic;
     }
 
+    /**
+     * @param techniqueStatistic technique statistic map
+     */
     public void setTechniqueStatistic(Map<String, Integer> techniqueStatistic) {
         this.techniqueStatistic = techniqueStatistic;
     }
