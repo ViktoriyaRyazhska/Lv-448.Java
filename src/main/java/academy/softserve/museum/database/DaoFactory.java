7adedd08f91b7e89e53457834a72e39e8a7f9930
Oleh Kopylchak <oleg12311999@gmail.com>
2019-10-23 16:09:38 +0300

    Added JavaDocs for DaoFactory class

--- src/main/java/academy/softserve/museum/database/DaoFactory.java
+++ src/main/java/academy/softserve/museum/database/DaoFactory.java
@@ -9,9 +9,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * Class that contains Connection and creates instances of DAO classes
+ */
 public final class DaoFactory {
+
+    /**
+     * Connection used for interaction with database
+     * This connection will be passed to every DAO instance
+     */
     private static final Connection connection;
 
+    /**
+     * Private constructor used for Singleton implementation
+     */
     private DaoFactory() {
     }
 
@@ -25,6 +36,11 @@ public final class DaoFactory {
         }
     }
 
+    /**
+     * Method for creating AuthorDao instance
+     *
+     * @return AuthorDao instance
+     */
     public static AuthorDao authorDao() {
         JdbcAuthorDao authorDao = JdbcAuthorDao.getInstance(connection);
         JdbcExhibitDao exhibitDao = JdbcExhibitDao.getInstance(connection, audienceDao());
@@ -35,14 +51,29 @@ public final class DaoFactory {
         return authorDao;
     }
 
+    /**
+     * Method for creating EmployeeDao instance
+     *
+     * @return EmployeeDao instance
+     */
     public static EmployeeDao employeeDao() {
         return JdbcEmployeeDao.getInstance(connection, audienceDao());
     }
 
+    /**
+     * Method for creating ExcursionDao instance
+     *
+     * @return ExcursionDao instance
+     */
     public static ExcursionDao excursionDao() {
         return JdbcExcursionDao.getInstance(connection);
     }
 
+    /**
+     * Method for creating ExhibitDao instance
+     *
+     * @return ExhibitDao instance
+     */
     public static ExhibitDao exhibitDao() {
         JdbcAuthorDao authorDao = JdbcAuthorDao.getInstance(connection);
         JdbcExhibitDao exhibitDao = JdbcExhibitDao.getInstance(connection, audienceDao());
@@ -53,14 +84,28 @@ public final class DaoFactory {
         return exhibitDao;
     }
 
+    /**
+     * Method for creating TimetableDao instance
+     *
+     * @return TimetableDao instance
+     */
     public static TimetableDao timetableDao() {
         return JdbcTimetableDao.getInstance(connection);
     }
 
+    /**
+     * Method for creating AudienceDao instance
+     *
+     * @return AudienceDao instance
+     */
     public static AudienceDao audienceDao() {
         return JdbcAudienceDao.getInstance(connection);
     }
 
+    /**
+     * Method for closing Connection. Should be called
+     * at the end of the program.
+     */
     public static void close() {
         try {
             connection.close();
