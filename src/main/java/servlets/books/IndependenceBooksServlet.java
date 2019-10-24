9a1ba5daaca2a190079d5ed6494d9012e92e3eb9
Yura Olkhovskyi <yuriy_olkhovskyi@i.ua>
2019-10-25 01:19:01 +0300

    fix

--- src/main/java/servlets/books/IndependenceBooksServlet.java
+++ src/main/java/servlets/books/IndependenceBooksServlet.java
@@ -3,14 +3,13 @@ package servlets.books;
 import service.BookService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 import java.time.LocalDate;
 
-@WebServlet("/independenceBooks")
 public class IndependenceBooksServlet extends HttpServlet {
     private BookService bookService;
 
