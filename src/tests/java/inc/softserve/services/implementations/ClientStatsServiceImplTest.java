f94c2b575719f16ecbe44aabcc1b4c860fb7398f
Turko Volodymyr <w.turko3000@gmail.com>
2019-10-25 17:43:41 +0300

    Add Mockito test and connect to test database.

--- src/tests/java/inc/softserve/services/implementations/ClientStatsServiceImplTest.java
+++ src/tests/java/inc/softserve/services/implementations/ClientStatsServiceImplTest.java
@@ -1,57 +1,48 @@
 package inc.softserve.services.implementations;
 
-import inc.softserve.dao.implementations.*;
 import inc.softserve.dao.interfaces.*;
-import inc.softserve.connectivity.ConnectDb;
 import inc.softserve.entities.Usr;
 import inc.softserve.services.intefaces.ClientStatsService;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.sql.Connection;
-import java.sql.SQLException;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class ClientStatsServiceImplTest {
 
-    private static BookingDao bookingDao;
-    private static RoomDao roomDao;
-    private static HotelDao hotelDao;
-    private static CityDao cityDao;
-    private static CountryDao countryDao;
-    private static UsrDao usrDao;
-    private static ClientStatsService clientStatsService;
-    private static VisaDao visaDao;
-    private static Connection connection;
+    @Mock
+    private BookingDao bookingDao;
+    @Mock
+    private CountryDao countryDao;
+    @Mock
+    private UsrDao usrDao;
+    @Mock
+    private ClientStatsService clientStatsService;
+    @Mock
+    private VisaDao visaDao;
 
-    @BeforeAll
-    static void init() throws SQLException {
-        connection = ConnectDb.connectBase();
-        countryDao = new CountryDaoJdbc(connection);
-        cityDao = new CityDaoJdbc(connection, countryDao);
-        hotelDao = new HotelDaoJdbc(connection, cityDao);
-        roomDao = new RoomDaoJdbc(connection, hotelDao);
-        usrDao = new UsrDaoJdbc(connection);
-        bookingDao = new BookingDaoJdbc(connection, usrDao, roomDao, hotelDao);
-        visaDao = new VisaDaoJdbc(connection, usrDao, countryDao);
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
         clientStatsService = new ClientStatsServiceImpl(usrDao, countryDao, visaDao, bookingDao);
     }
 
     @Test
-    void getUserByEmailTest() {
-        Usr user = usrDao
-                .findByEmail("igor@gmail.com")
-                .orElseThrow();
-        Usr expected = clientStatsService.getUser("igor@gmail.com").get();
-        assertEquals(expected, user);
+    void getUserByEmailTest1() {
+        Usr user = new Usr();
+        user.setEmail("igor@gmail.com");
+        when(usrDao.findByEmail("igor@gmail.com")).thenReturn(Optional.of(user));
+        assertEquals(clientStatsService.getUser("igor@gmail.com"), Optional.of(user));
     }
 
     @Test
-    void getUserByInvalidEmailTest() throws Exception {
-        Optional<Usr> user = usrDao
-                .findByEmail("i@gmail.com");
-        Optional<Usr> expected = clientStatsService.getUser("i@gmail.com");
-        assertEquals(expected, user);
+    void getUserByInvalidEmailTest1() {
+        when(usrDao.findByEmail("igorgmail.com")).thenReturn(Optional.of(new Usr()));
+        assertThrows(NullPointerException.class, () -> clientStatsService.getUser("igogmail.com"));
     }
 }
