6a2ac2e814bcf9f314c9e0a7097243aa28bca63a
nomadness <vitalii.skolozdra@gmail.com>
2019-10-28 08:53:23 +0200

    Add javadoc

--- src/main/java/academy/softserve/museum/servlet/IndexServlet.java
+++ src/main/java/academy/softserve/museum/servlet/IndexServlet.java
@@ -7,8 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Class processes requests for "/" (/index) url
+ *
+ * @version 1.0
+ */
 @WebServlet(value = {""})
 public class IndexServlet extends HttpServlet {
+
+    /**
+     * Method processes GET request for /index url
+     * and returns /index.jsp (homepage)
+     *
+     * @param req  HTTP request object
+     * @param resp HTTP response object
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/index.jsp").include(req, resp);
--- src/main/java/academy/softserve/museum/servlet/author/AddAuthorServlet.java
+++ src/main/java/academy/softserve/museum/servlet/author/AddAuthorServlet.java
@@ -1,7 +1,5 @@
 package academy.softserve.museum.servlet.author;
 
-import academy.softserve.museum.constant.MessageType;
-import academy.softserve.museum.entities.Author;
 import academy.softserve.museum.entities.dto.AuthorDto;
 import academy.softserve.museum.services.AuthorService;
 import academy.softserve.museum.services.impl.AuthorServiceImpl;
@@ -13,23 +11,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Class processes requests for /exhibits/add-author url.
+ *
+ * @version 1.0
+ */
 @WebServlet("/exhibits/add-author")
 public class AddAuthorServlet extends HttpServlet {
 
     private AuthorService authorService;
 
+    /**
+     * Method initializes required resources
+     */
     @Override
-    public void init() throws ServletException {
+    public void init() {
         authorService = new AuthorServiceImpl();
     }
 
+    /**
+     * Method processes GET request for /exhibits/add-author url
+     * and returns /add-author.jsp form
+     * for adding new authors.
+     *
+     * @param req HTTP request object
+     * @param resp HTTP response object
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/add-author.jsp").include(req, resp);
     }
 
+    /**
+     * Method processes POST request for /exhibits/add-author url
+     * gets parameters from request object,
+     * creates new AuthorDto and passes it to service layer.
+     *
+     * @param req HTTP request object
+     * @param resp HTTP response object
+     * @throws IOException
+     */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try {
             String firstname = req.getParameter("firstname");
             String lastname = req.getParameter("lastname");
